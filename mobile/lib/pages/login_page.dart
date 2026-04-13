c69fa90338d8eda1e034c5c78090e92d32f05317
thetomy777 <166961696+thetomy777@users.noreply.github.com>
2026-04-13 15:01:50 -0300

    tela login - estatica sem efeito

---
+++ mobile/lib/pages/login_page.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/componentes/botao_login.dart';
+import 'package:mobile/componentes/campo_de_texto.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // nao vai fazer nada por agora pq ta sem tela do menu principal
+  void loginUser() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea( // safearea faz com que o icone nao fique na area da bateria ou do wifi ou coisa do tipo
+        child: Center( // como o nome diz centraliza no meio
+          child: Column(
+            children: [
+              SizedBox(height: 80,),
+
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey[400],
+              ),
+
+              SizedBox(height: 10,),
+
+              Text(
+                'Fazer o Login',
+                style: GoogleFonts.lora(
+                  fontSize: 30,
+                  color: Colors.purple[900],
+                ),
+              ),
+
+              SizedBox(height: 30,),
+
+              Image.asset('lib/images/perfil.png'),
+
+              SizedBox(height: 60,),
+
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 30, bottom: 10), 
+                    child: Text(
+                      'E-mail',
+                      style: GoogleFonts.lora(
+                        fontSize: 20,
+                      ),
+                      ),
+                  ),
+                ],
+              ),
+
+              CampoDeTexto(
+                controller: usernameController,
+                hintText: 'Minhaconta@gmail.com',
+                obscureText: false,
+              ),
+
+              SizedBox(height: 10,),
+
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 30, bottom: 10), 
+                    child: Text(
+                      'Senha',
+                      style: GoogleFonts.lora(
+                        fontSize: 20,
+                      ),
+                      ),
+                  ),
+                ],
+              ),
+
+              CampoDeTexto(
+                controller: passwordController,
+                hintText: 'Senha',
+                obscureText: true,
+              ),
+
+              SizedBox(height: 30,),
+
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 30, bottom: 10), 
+                    child: Text(
+                      'Esqueci minha senha',
+                      style: GoogleFonts.lora(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 2.5,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 50,),
+
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Image.asset(
+                      'lib/images/Checkmark.png',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      'Lembrar de mim',
+                      style: GoogleFonts.lora(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 30,),
+
+              Divider(
+                thickness: 0.5,
+                color: Colors.grey[400],
+              ),
+
+              SizedBox(height: 20,),
+
+              Text.rich(
+                TextSpan(
+                  text: 'Não tenho cadastro. ',
+                  style: TextStyle(
+                    color: Colors.black
+                  ),
+                children: [
+                  TextSpan(
+                    text: 'Cadastrar',
+                    style: TextStyle(
+                      color: Colors.blue[600],
+                      fontWeight: FontWeight.bold,
+                    )
+                  )
+                ]
+                )
+              ),
+
+              SizedBox(height: 30,),
+
+              BotaoLogin(
+                onTap: loginUser,
+              )
+              
+          ],),
+        ),
+      ),
+    );
+  }
+}
