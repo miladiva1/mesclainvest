c3e92a80ada714e0c75c8e1198eb061cebc4042e
camilarevth <camilafcostacurta@gmail.com>
2026-04-22 18:22:57 -0300

    Update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,40 +1,28 @@
-/*Não escreva código nesta tela, use o caminho que eu instruí por email! 
-cd lib/features/startups/presentation/screen
-E dentro de screen (telas), é onde você vai desenvolver sua tela que eu tiver 
-atribuído a você!
-*/
-
-
-
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart'; // Será usado quando conectarmos a nuvem real
+import 'package:flutter_application_1/features/startups/presentation/screens/criar_um.dart';
+import 'package:flutter_application_1/features/startups/presentation/screens/wallet_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // A inicialização real do Firebase ficará aqui na próxima etapa de infraestrutura
-  
-  runApp(const MesclaInvestApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MesclaInvestApp extends StatelessWidget {
-  const MesclaInvestApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MesclaInvest',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("MesclaInvest - Modelo Base")),
-        body: const Center(
-          child: Text(
-          
-            "Este é apenas um Molde.\nSubstitua esta tela pela sua Feature.",
-            textAlign: TextAlign.center,
-          ),
-        ),
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
       ),
+      routes: {
+        '/mfa': (context) => const MfaScreen(),
+        '/wallet': (context) => const CarteiraBalcaoScreen(),
+      },
+      home: const MfaScreen(),
     );
   }
 }
